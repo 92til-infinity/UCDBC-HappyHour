8c331f5e64dd80a91a6e70347db3b618ed09bb52
Ryan Skog <ryan.skog@outlook.com>
2020-05-12 21:57:03 -0700

    Updating the main.js to include Zomato and MapBox API

--- javascript/main.js
+++ javascript/main.js
@@ -104,3 +104,162 @@ $.ajax({
 
 getBeerList();
 console.log(responseADDRESS);
+
+
+// MAPBOX JS
+
+mapboxgl.accessToken = "pk.eyJ1IjoicmFza29nIiwiYSI6ImNrOXhvOTdleTA1bHozbXBtc3R0ZDVkODIifQ.706vwlV4IYaDY7ZTOLEt9w";
+
+let end = [userLongitude, userLatitude];
+let marker = new mapboxgl.Marker();
+
+let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v10",
+    center: [-121.4855, 38.5624],
+    zoom: 1
+});
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+})
+
+map.addControl(geolocate);
+
+map.on("load", function () {
+    geolocate.trigger();
+    start = map.getCenter();
+})
+
+geolocate.on("geolocate", function (position) {
+    // console.log(position);
+    const coords = [position.coords.longitude, position.coords.latitude];
+    marker.setLngLat(coords).addTo(map);
+})
+
+
+let option1 = new mapboxgl.Marker()
+    .setLngLat([-121.492070, 38.562010])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Tower Cafe</h1>"))
+    .addTo(map);
+let option2 = new mapboxgl.Marker()
+    .setLngLat([-121.475563, 38.556201])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Brewing Company Place</h1>"))
+    .addTo(map);
+let option3 = new mapboxgl.Marker()
+    .setLngLat([-121.465655, 38.564541])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>The Hideaway</h1>"))
+    .addTo(map);
+let option4 = new mapboxgl.Marker()
+    .setLngLat([-121.442757, 38.560544])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>The Shack</h1>"))
+    .addTo(map);
+let option5 = new mapboxgl.Marker()
+    .setLngLat([-121.425975, 38.555080])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Dos Coyotes</h1>"))
+    .addTo(map);
+
+
+map.on('load', function () {
+
+    map.addLayer({
+        id: 'point',
+        type: 'circle',
+        source: {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [{
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        type: 'Point',
+                        coordinates: start
+                    }
+                }
+                ]
+            }
+        },
+        paint: {
+            'circle-radius': 10,
+            'circle-color': '#3887be'
+        }
+    });
+})
+
+let canvas = map.getCanvasContainer();
+
+$("#clickMe").on("click", function () {
+
+    const start = marker.getLngLat();
+    console.log(start);
+
+    let targetURL = "https://api.mapbox.com/directions/v5/mapbox/cycling/" + start.lng + ","
+        + start.lat + ";" + end[0] + "," + end[1] + "?steps=true&geometries=geojson&access_token="
+        + mapboxgl.accessToken;
+
+    var req = new XMLHttpRequest();
+    req.open("GET", targetURL, true);
+    req.onload = function () {
+
+        let json = JSON.parse(req.response);
+        let data = json.routes[0];
+        let route = data.geometry.coordinates;
+        let geojson = {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                type: "LineString",
+                coordinates: route
+            }
+        };
+
+        // If the route already exists on the map
+        if (map.getSource("route")) {
+            map.getSource("route").setData(geojson);
+        } else {
+            map.addLayer({
+                id: "route",
+                type: "line",
+                source: {
+                    type: "geojson",
+                    data: {
+                        type: "Feature",
+                        properties: {},
+                        geometry: {
+                            type: "LineString",
+                            coordinates: geojson
+                        }
+                    }
+                },
+                layout: {
+                    "line-join": "round",
+                    "line-cap": "round"
+                },
+                paint: {
+                    "line-color": "#3887be",
+                    "line-width": 5,
+                    "line-opacity": 0.75
+                }
+            });
+        }
+        // add turn instruction here
+        const instructions = $("#instructions");
+        let steps = data.legs[0].steps;
+
+        let tripInstructions = [];
+        for (let i = 0; i < steps.length; i++) {
+            tripInstructions.push("<br><li>" + steps[i].maneuver.instruction) + "</li>";
+            $("#instructions").html("<br><span class='duration'>Trip duration: "
+                + Math.floor(data.duration / 60) + " min 🚴 </span>" + tripInstructions);
+        }
+    };
+    req.send();
+})
+
+
+
+
