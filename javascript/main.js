cc3d9bc7167ea7de4eee0509eb342b67528c484d
Ryan Skog <ryan.skog@outlook.com>
2020-05-15 12:28:23 -0700

    Fixed centering issue, marker colors, popups

--- javascript/main.js
+++ javascript/main.js
@@ -1,122 +1,98 @@
-// var userInput;
+var currentMarkers = [];
 
-var userLongitude;
-var userLatitude;
-
-
+$(".instructionList").hide();
 $(".buttonDesign").on("click", function () {
     event.preventDefault();
+
     const request = $(this).attr("id");
     $("#locale-buttons").html("");
-    console.log(request);
 
-    // userInput = $("#searchInput").val().trim();
-    // console.log(userInput);
-    if (request !== "") {
-        //clear the previous search
-        clear();
-        //clear the search field value
-        $("#searchinput").val("");
-    }
     getBeerList(request);
 });
 
 function getBeerList(request) {
 
-    //get 5 day forecast
-    var zomatoURL = "https://developers.zomato.com/api/v2.1/search?q=" + request + "&count=5&lat=" + userLatitude + "&lon=" + userLongitude;
-    // console.log(zomatoURL);
+    var geoURL = "https://api.ipgeolocation.io/ipgeo?apiKey=9b336091095743018515a967edc697aa&fields=geo"
     $.ajax({
-        url: zomatoURL,
-        method: "GET",
-        headers: { "user-key": "4d08f1088ece3e99c9b69a373a097eed" }
-        // curl - X GET--header "Accept: application/json" --header "user-key: 4d08f1088ece3e99c9b69a373a097eed" "https://developers.zomato.com/api/v2.1/search"
+        url: geoURL,
+        method: "GET"
     })
         .then(function (response) {
-            // ________________________
-            var responseADDRESS = [];
-            // ________________________
-            //add container div for forecast cards
-            // let newLIST = $("<ul>").addClass("list-group");
-            // $("#locale-buttons").append(newLIST);
-            console.log(response);
+            const userLongitude = response.longitude;
+            const userLatitude = response.latitude;
 
-            //loop through array response to find the names and addresses of each restauarant for 15:00
-            for (let i = 0; i < response.restaurants.length; i++) {
-                console.log(response.restaurants[i].restaurant.name);
-                const restNAME = response.restaurants[i].restaurant.name;
-                const restADDRESS = response.restaurants[i].restaurant.location.address;
-                const restCITY = response.restaurants[i].restaurant.location.city;
-                const restPRICE = response.restaurants[i].restaurant.price_range;
-                // in 1 - 5 scale symbols
-                const restREVIEW = response.restaurants[i].restaurant.user_rating.aggregate_rating;
-                // in user review integer
-                const responseLATLON = {
-                    latitude: response.restaurants[i].restaurant.location.latitude,
-                    longitude: response.restaurants[i].restaurant.location.longitude,
-                    name: response.restaurants[i].restaurant.name
-                };
+            //get 5 day forecast
+            var zomatoURL = "https://developers.zomato.com/api/v2.1/search?q=" + request + "&count=5&lat=" + userLatitude + "&lon=" + userLongitude;
+            // console.log(zomatoURL);
+            $.ajax({
+                url: zomatoURL,
+                method: "GET",
+                headers: { "user-key": "4d08f1088ece3e99c9b69a373a097eed" }
+                // curl - X GET--header "Accept: application/json" --header "user-key: 4d08f1088ece3e99c9b69a373a097eed" "https://developers.zomato.com/api/v2.1/search"
+            })
+                .then(function (response) {
+                    // ________________________
+                    var responseADDRESS = [];
+                    // ________________________
+                    //add container div for forecast cards
+                    // let newLIST = $("<ul>").addClass("list-group");
+                    // $("#locale-buttons").append(newLIST);
+                    console.log(response);
 
-                responseADDRESS.push(responseLATLON);
+                    //loop through array response to find the names and addresses of each restauarant for 15:00
+                    for (let i = 0; i < response.restaurants.length; i++) {
+                        console.log(response.restaurants[i].restaurant.name);
+                        const restNAME = response.restaurants[i].restaurant.name;
+                        // const restADDRESS = response.restaurants[i].restaurant.location.address;
+                        // const restCITY = response.restaurants[i].restaurant.location.city;
+                        const restPRICE = response.restaurants[i].restaurant.price_range;
+                        // in 1 - 5 scale symbols
+                        const restREVIEW = response.restaurants[i].restaurant.user_rating.aggregate_rating;
+                        // in user review integer
+                        const responseLATLON = {
+                            latitude: response.restaurants[i].restaurant.location.latitude,
+                            longitude: response.restaurants[i].restaurant.location.longitude,
+                            name: response.restaurants[i].restaurant.name
+                        };
 
-                const listBodyText = $("<button>").addClass("list-group-item list-group-item-action list-text")
-                    .text(restNAME);
-                const goButton = $("<button>").addClass("goButton").attr("id", i).text("GO");
-                $(listBodyText).append(goButton);
-                $("#locale-buttons").append(listBodyText);
+                        responseADDRESS.push(responseLATLON);
 
-                //listBodyText.append($("<p>").addClass("list-text").text(restNAME);
-                // listBodyText.append($("<p>").attr("class", "list-text").html("address: " + restADDRESS + "."));
-                // listBodyText.append($("<p>").attr("class", "list-text").html("city: " + restCITY + "."));
-                listBodyText.append($("<p>").attr("class", "list-text").html("Price: " + restPRICE + "/5"));
-                listBodyText.append($("<p>").attr("class", "list-text").html("Avg Review: " + restREVIEW));
+                        const listBodyText = $("<li>").addClass("list-group-item list-group-item-active list-text")
+                            .attr("id", i).text(restNAME);
+                        const goButton = $("<button>").addClass("goButton").attr("id", i).text("GO");
+                        $(listBodyText).append(goButton);
+                        $("#locale-buttons").append(listBodyText);
 
-            }
-            localStorage.setItem("responseADDRESS", JSON.stringify(responseADDRESS));
-            mapMarkers(responseADDRESS);
-        });
+                        //listBodyText.append($("<p>").addClass("list-text").text(restNAME);
+                        // listBodyText.append($("<p>").attr("class", "list-text").html("address: " + restADDRESS + "."));
+                        // listBodyText.append($("<p>").attr("class", "list-text").html("city: " + restCITY + "."));
+                        listBodyText.append($("<p>").text("Price: " + restPRICE + "/5"));
+                        listBodyText.append($("<p>").text("Avg Review: " + restREVIEW));
+
+                    }
+                    localStorage.setItem("responseADDRESS", JSON.stringify(responseADDRESS));
+                    clear();
+                    mapMarkers(responseADDRESS);
+
+                });
+        })
 }
 
 // ________________________
 
 function clear() {
-    $("#searchInput").empty();
-    $("#listgroup").empty();
+    if (currentMarkers !== null) {
+        for (var i = currentMarkers.length - 1; i >= 0; i--) {
+            currentMarkers[i].remove();
+        }
+    }
 }
 
-// ____________________
-
-// function addPointsToMap(responseADDRESS) {
-//     for (var i = 0; i < responseADDRESS.length; i++) {
-//         var mapPoints = responseADDRESS[i];
-//         console.log(mapPoints[0], mapPoints[1]);
-//     }
-// }
-
-
-// ________________________ // ________________________
-// var geoURL = "https://api.ipgeolocation.io/ipgeo?apiKey=9b336091095743018515a967edc697aa&fields=geo"
-// $.ajax({
-//     url: geoURL,
-//     method: "GET"
-
-// })
-//     .then(function (response) {
-//         console.log(response);
-//         console.log(response.latitude);
-//         console.log(response.longitude);
-//         userLongitude = response.longitude;
-//         userLatitude = response.latitude;
-//     });
-// ________________________ // ________________________ // ________________________
-
-//getBeerList();
-//console.log(responseADDRESS);
 
 mapboxgl.accessToken = "pk.eyJ1IjoicmFza29nIiwiYSI6ImNrOXhvOTdleTA1bHozbXBtc3R0ZDVkODIifQ.706vwlV4IYaDY7ZTOLEt9w";
 
-let end = [userLongitude, userLatitude];
 let marker = new mapboxgl.Marker();
+let restBox = [];
 
 let map = new mapboxgl.Map({
     container: "map",
@@ -147,52 +123,95 @@ geolocate.on("geolocate", function (position) {
 
 // Create map markers for the five returned searches
 function mapMarkers(restArray) {
+
     for (i = 0; i < restArray.length; i++) {
-        let newMark = new mapboxgl.Marker()
+
+        let newMark = new mapboxgl.Marker({
+            color: "darkslategrey"
+        })
             .setLngLat([restArray[i].longitude, restArray[i].latitude])
-            .setPopup(new mapboxgl.Popup().setHTML("<h6>" + restArray[i].name + "</h6>"))
+            .setPopup(new mapboxgl.Popup().setHTML("<p>" + restArray[i].name + "</p>"))
             .addTo(map);
+
+        $(newMark).addClass("destinations");
+        currentMarkers.push(newMark);
     }
 }
 
-map.on('load', function () {
+// map.on('load', function () {
 
-    map.addLayer({
-        id: 'point',
-        type: 'circle',
-        source: {
-            type: 'geojson',
-            data: {
-                type: 'FeatureCollection',
-                features: [{
-                    type: 'Feature',
-                    properties: {},
-                    geometry: {
-                        type: 'Point',
-                        coordinates: start
-                    }
-                }
-                ]
-            }
-        },
-        paint: {
-            'circle-radius': 10,
-            'circle-color': '#3887be'
-        }
-    });
-})
+//     map.addLayer({
+//         id: 'point',
+//         type: 'circle',
+//         source: {
+//             type: 'geojson',
+//             data: {
+//                 type: 'FeatureCollection',
+//                 features: [{
+//                     type: 'Feature',
+//                     properties: {},
+//                     geometry: {
+//                         type: 'Point',
+//                         coordinates: start
+//                     }
+//                 }
+//                 ]
+//             }
+//         },
+//         paint: {
+//             'circle-radius': 10,
+//             'circle-color': '#3887be'
+//         }
+//     });
+// })
 
 //let canvas = map.getCanvasContainer();
 
-$("body").on("click", ".goButton", function () {
+function fillBox() {
+    if (restBox.length == 0) {
+        restBox = JSON.parse(localStorage.getItem("responseADDRESS"));
+        console.log("This array is empty");
+    } else {
+        return;
+    }
+}
+
+$("body").on("click", ".list-text", function (event) {
+
+    event.stopPropagation();
+    fillBox();
+
+    const getIndex = $(this).attr("id");
+    const endLat = restBox[getIndex].latitude;
+    const endLon = restBox[getIndex].longitude;
+    map.setCenter([endLon, endLat]);
+    currentMarkers[getIndex].togglePopup();
+})
+
+$("body").on("click", ".goButton", function (event) {
 
     event.stopPropagation();
 
+    $(".list-group-item ").hide();
+    $(".instructionList").show();
     const responseADDRESS = JSON.parse(localStorage.getItem("responseADDRESS"));
     const getIndex = $(this).attr("id");
-    const endLon = responseADDRESS[getIndex].longitude;
-    const endLat = responseADDRESS[getIndex].latitude;
+    const endLon = parseFloat(responseADDRESS[getIndex].longitude);
+    const endLat = parseFloat(responseADDRESS[getIndex].latitude);
     const start = marker.getLngLat();
+    const midpointLon = ((endLon + start.lng) / 2);
+    const midpointLat = ((endLat + start.lat) / 2);
+    map.setCenter([midpointLon, midpointLat]);
+    map.zoomTo(11);
+    clear();
+
+    // End destination marker after we clear all the markers
+    let endMark = new mapboxgl.Marker({
+        color: "darkslategrey"
+    })
+        .setLngLat([endLon, endLat])
+        .addTo(map);
+    currentMarkers.push(endMark);
 
     let targetURL = "https://api.mapbox.com/directions/v5/mapbox/cycling/" + start.lng + ","
         + start.lat + ";" + endLon + "," + endLat + "?steps=true&geometries=geojson&access_token="
@@ -201,7 +220,7 @@ $("body").on("click", ".goButton", function () {
     var req = new XMLHttpRequest();
     req.open("GET", targetURL, true);
     req.onload = function () {
-
+        console.log("onload() called")
         let json = JSON.parse(req.response);
         let data = json.routes[0];
         let route = data.geometry.coordinates;
@@ -212,7 +231,7 @@ $("body").on("click", ".goButton", function () {
                 type: "LineString",
                 coordinates: route
             }
-        };
+        }
 
         // If the route already exists on the map
         if (map.getSource("route")) {
@@ -242,21 +261,19 @@ $("body").on("click", ".goButton", function () {
                     "line-opacity": 0.75
                 }
             });
+            map.getSource("route").setData(geojson);
         }
         // add turn instruction here
-        const instructions = $("#instructions");
         let steps = data.legs[0].steps;
-
         let tripInstructions = [];
         for (let i = 0; i < steps.length; i++) {
-            tripInstructions.push("<br><li>" + steps[i].maneuver.instruction) + "</li>";
-            $("#instructions").html("<br><span class='duration'>Trip duration: "
+
+            tripInstructions.push("<br></br>" + "<li>" + steps[i].maneuver.instruction + " ");
+            $(".Trip").html("<br><span class='duration'>Trip duration: "
                 + Math.floor(data.duration / 60) + " min 🚴 </span>" + tripInstructions);
         }
     };
     req.send();
 })
-
-
 
 
